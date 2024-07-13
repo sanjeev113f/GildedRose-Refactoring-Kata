fd76955b71d1399d188fb35c716a04e0b4521cc4
sanjeev113f <sanjeevk@sahaj.ai>
2024-07-13 15:38:43 +0530

    Add functions in Item class

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -4,4 +4,17 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+
+    fun degradeQuality(value: Int) {
+        this.quality = (this.quality - value).takeIf { it >= 0 } ?: 0
+    }
+
+    fun upgradeQuality(value: Int) {
+        this.quality = (this.quality + value).takeIf { it <= 50 } ?: 50
+    }
+
+    fun degradeSellIn() {
+        this.sellIn -= 1
+    }
+
 }
