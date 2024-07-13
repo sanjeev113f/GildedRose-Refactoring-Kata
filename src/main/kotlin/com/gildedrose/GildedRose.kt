0fc79f9b6ce899afc12a12af2323a5cb62c7c459
sanjeev113f <sanjeevk@sahaj.ai>
2024-07-13 15:51:40 +0530

    Merge code into a sinle when block

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,27 +6,26 @@ class GildedRose(var items: List<Item>) {
         for (item in items) {
             when (item.name) {
                 "Sulfuras, Hand of Ragnaros" -> continue
-                "Aged Brie" -> item.upgradeQuality(1)
+                "Aged Brie" -> {
+                    item.upgradeQuality(1)
+                    if (item.sellIn < 1) item.upgradeQuality(1)
+                }
+
                 "Backstage passes to a TAFKAL80ETC concert" -> {
                     item.upgradeQuality(1)
                     if (item.sellIn < 11) item.upgradeQuality(1)
                     if (item.sellIn < 6) item.upgradeQuality(1)
+                    if (item.sellIn < 1) item.degradeQuality(item.quality)
                 }
 
-                else -> item.degradeQuality(1)
+                else -> {
+                    item.degradeQuality(1)
+                    if (item.sellIn < 1) item.degradeQuality(1)
+                }
             }
 
             item.degradeSellIn()
-
-            if (item.sellIn < 0) {
-                when (item.name) {
-                    "Aged Brie" -> item.upgradeQuality(1)
-                    "Backstage passes to a TAFKAL80ETC concert" -> item.degradeQuality(item.quality)
-                    else -> item.degradeQuality(1)
-                }
-            }
         }
     }
-
 }
 
