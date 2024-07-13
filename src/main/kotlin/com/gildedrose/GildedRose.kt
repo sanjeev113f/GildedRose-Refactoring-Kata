f7bb47824ca826f6eb0396056a9f6c32b944e3d5
sanjeev113f <sanjeevk@sahaj.ai>
2024-07-13 15:40:27 +0530

    Refactor Code - Replace conditional statements with When block

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,51 +4,25 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
+            when (item.name) {
+                "Sulfuras, Hand of Ragnaros" -> continue
+                "Aged Brie" -> item.upgradeQuality(1)
+                "Backstage passes to a TAFKAL80ETC concert" -> {
+                    item.upgradeQuality(1)
+                    if (item.sellIn < 11) item.upgradeQuality(1)
+                    if (item.sellIn < 6) item.upgradeQuality(1)
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
+                else -> item.degradeQuality(1)
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
+            item.degradeSellIn()
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
+                when (item.name) {
+                    "Aged Brie" -> item.upgradeQuality(1)
+                    "Backstage passes to a TAFKAL80ETC concert" -> item.degradeQuality(item.quality)
+                    else -> item.degradeQuality(1)
                 }
             }
         }
