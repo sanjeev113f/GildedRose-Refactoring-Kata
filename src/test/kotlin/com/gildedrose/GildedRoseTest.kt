64633065f1d58952ab77875ff3e1bfc0ecf397a8
sanjeev113f <sanjeevk@sahaj.ai>
2024-07-13 12:16:26 +0530

    Add test before refactoring

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -3,8 +3,6 @@ package com.gildedrose
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 internal class GildedRoseTest {
 
@@ -15,5 +13,75 @@ internal class GildedRoseTest {
         app.updateQuality()
         assertEquals("foo", app.items[0].name)
 
+    }
+
+    @Test
+    fun `For more than Ten days updates`() {
+        val days = 12
+        val items = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        )
+        val expectedResponse = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 3, 41),
+        )
+
+        val app = GildedRose(items)
+        for (i in 0 until days) app.updateQuality()
+
+        assertThat(expectedResponse).usingRecursiveFieldByFieldElementComparatorIgnoringFields().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `For 5 days update`() {
+        val days = 5
+        val items = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        )
+        val expectedResponse = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 25),
+        )
+
+        val app = GildedRose(items)
+        for (i in 0 until days) app.updateQuality()
+
+        assertThat(expectedResponse).usingRecursiveFieldByFieldElementComparatorIgnoringFields().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `For 7 days update`() {
+        val days = 7
+        val items = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        )
+        val expectedResponse = listOf(
+            Item("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 8, 29),
+        )
+
+        val app = GildedRose(items)
+        for (i in 0 until days) app.updateQuality()
+
+        assertThat(expectedResponse).usingRecursiveFieldByFieldElementComparatorIgnoringFields().isEqualTo(app.items)
+    }
+
+    @Test
+    fun `Max quality test`() {
+        val days = 15
+        val items = listOf(
+            Item("Aged Brie", 2, 40), Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        )
+        val expectResponse = listOf(
+            Item("Aged Brie", -13, 50), Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        )
+
+        val app = GildedRose(items)
+        for (i in 0 until days) app.updateQuality()
+
+        assertThat(expectResponse).usingRecursiveComparison().isEqualTo(app.items)
     }
 }
