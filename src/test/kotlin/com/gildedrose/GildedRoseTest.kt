d7b5a8184a3e6b6bae7333d7146669eb00eabab1
sanjeev113f <sanjeevk@sahaj.ai>
2024-07-13 12:04:25 +0530

    Intial setup

---
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,19 @@
+package com.gildedrose
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class GildedRoseTest {
+
+    @Test
+    fun foo() {
+        val items = listOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("foo", app.items[0].name)
+
+    }
+}
